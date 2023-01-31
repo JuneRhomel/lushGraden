a5c42ffebfec95e49e9fab6c89dd4e1d8c3adbef
June Rhomel Mandigma <104764941+JuneRhomel@users.noreply.github.com>
2023-01-31 10:17:25 +0800

    gird

--- garden/src/components/Service.jsx
+++ garden/src/components/Service.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Banner from "../assets/Group 1.png"
 import Cosuming from "../assets/Time-Cosuming 1.svg"
+import Sprout from "../assets/Grow_Sprout 1.svg"
+import Temperature from "../assets/Temperature 1.svg"
+import Pruning from "../assets/Pruning 1.svg"
 import "../style/service.css"
 
 export default function Service() {
@@ -8,35 +11,45 @@ export default function Service() {
         <section className='service-section'>
             <div className="">
                 <div className='grid-service '>
-                <img className='left-img box1' src={Banner} alt="" />
-                <div className='box2 box'>
-                    <img src={Cosuming} alt="" />
-                    <div>
-                        <h3>Quality Product</h3>
-                        <p>Our flowers are of the highest quality, carefully selected and sourced from reput able</p>
+
+                    <img className='left-img box1' src={Banner} alt="" />
+
+                    <div className='box2 box'>
+                        <div className='service-box'>
+                            <div><img src={Cosuming} alt="" /></div>
+                            <div>
+                                <h3>Quality Product</h3>
+                                <p>Our flowers are of the highest quality, carefully selected and sourced from reput able</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div  className='box3 box'>
-                    <div><img src={Cosuming} alt="" /></div>
-                    <div>
-                        <h3>Quality Product</h3>
-                        <p>Our flowers are of the highest quality, carefully selected and sourced from reput able</p>
+                    <div className='box3 box'>
+                        <div  className='service-box'>
+                            <div><img src={Sprout} alt="" /></div>
+                            <div>
+                                <h3>Quality Product</h3>
+                                <p>Our flowers are of the highest quality, carefully selected and sourced from reput able</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div  className='box4 box'>
-                    <img src={Cosuming} alt="" />
-                    <div>
-                        <h3>Quality Product</h3>
-                        <p>Our flowers are of the highest quality, carefully selected and sourced from reput able</p>
+                    <div className='box4 box'>
+                        <div  className='service-box'>
+                            <div><img src={Temperature} alt="" /></div>
+                            <div>
+                                <h3>Always Fresh</h3>
+                                <p>Our flowers are of the highest quality, carefully selected and sourced from reput able</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div  className='box5 box'>
-                    <img src={Cosuming} alt="" />
-                    <div>
-                        <h3>Quality Product</h3>
-                        <p>Our flowers are of the highest quality, carefully selected and sourced from reput able</p>
+                    <div className='box5 box'>
+                        <div  className='service-box'>
+                            <div><img src={Pruning} alt="" /></div>
+                            <div>
+                                <h3>Quality Product</h3>
+                                <p>Our flowers are of the highest quality, carefully selected and sourced from reput able</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
                 </div>
             </div>
 
