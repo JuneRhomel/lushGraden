44ebe7f307bded8773c04b29749b667edcc953e5
June Rhomel <junemandigma@gmailo.com>
2023-02-12 12:37:32 +0800

    MOdel

--- garden/src/components/Modem.jsx
+++ garden/src/components/Modem.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
+import Star from "../assets/Star 1.svg"
 
-export default function Modem({value,closeBtn}) {
+export default function Modem({ value, closeBtn }) {
   return (
     <div className='Modem-bg'>
-    <div className='Modem'>
-        <button onClick={closeBtn}>Close</button>
-        <img src={value.img} alt="" />
-        <h3>{value.title}</h3>
-        <small>(${value.originPrize})</small>
-        <b>${value.prize}</b>
-    </div>
+      <div className='Modem'>
+        <div className='all-img'>
+          <img className='main-img' src={value.img} alt="" />
+          <div className='sub-img'>
+            <img src={value.img} alt="" />
+            <img src={value.img} alt="" />
+            <img src={value.img} alt="" />
+          </div>
+        </div>
+
+        <div className='desctiption'>
+          {/* <button onClick={closeBtn}>Close</button> */}
+          <div className='head'>
+            <div className='title'>
+              <h3>{value.title}</h3>
+              <div className='img-star'>
+                <img src={Star} alt="" />
+                <img src={Star} alt="" />
+                <img src={Star} alt="" />
+                <img src={Star} alt="" />
+              </div>
+              <div>
+                <small>(${value.originPrize})</small>
+                <b>${value.prize}</b>
+              </div>
+            </div>
+            <button>Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
